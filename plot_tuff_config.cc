4e2d0292524c157fd061ea7fffd9fdb8fdb25695
keithwm2165 <keithwm2165@gmail.com>
2017-10-10 17:05:38 -0400

    Began working on a test script in root for plotting the configuration of TUFF from an argument passed to the script

---
+++ plot_tuff_config.cc
@@ -0,0 +1,45 @@
+// This is the root script to plot on a TProfile the configurations from the csv files. 
+#include <fstream>
+#include <iostream>
+#include <TProfile>
+
+using namespace std;
+
+
+void plot_tuff_config()
+{
+// string for the configuration.
+     string config_name = "config";
+     string config_name_file = arg_1;
+
+// Declare the file to open for the tuff configuration
+     ifstream config_infile;
+     config_infile.open("config_name_file");
+
+
+     TProfile *config_dbr = new TProfile ("TUFF config dbr", "TUFF configuration dbr", 100, 0, 10);
+     TProfile *config_dbi = new TProfile ("TUFF config dbi", "TUFF configuration dbi", 100, 0, 10);
+     TProfile *config_p_r = new TProfile ("TUFF config phase r", "TUFF configuration phase r", 100, 0, 10);
+     TProfile *config_p_i = new TProfile ("TUFF config phase i", "TUFF configuration phase i", 100, 0, 10);
+
+     double freq = 0;
+     double dbr = 0;
+     double dbi = 0;
+     double p_r = 0;
+     double p_i = 0;
+
+     for (int i = 0; i < length; i++)
+     {
+          config_infile >> freq >> dbr >> dbi;
+          config_dbr->Fill(freq,dbr);
+          config_dbi->Fill(freq,dbi);
+          config_p_r->Fill(freq,p_r);
+          config_p_i->Fill(freq,p_i);
+
+     }
+
+
+// Draw the plots
+     config_dbr->Draw();
+     c1->SaveAs(".png");
+}
